317db9a08c307e3f912cd6551cb50a7a1f53df03
Sidhik <sidhik64742@gmail.com>
2025-03-08 15:32:03 +0530

    updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/project-management/" >
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/projects" element={<Projects />} />
